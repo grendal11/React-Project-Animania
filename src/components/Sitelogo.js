cc85c1d29ad63f394559a281d0c67c1351ca3628
Valentina Ivanova <40993628+grendal11@users.noreply.github.com>
2021-12-08 14:50:18 +0200

    Added Site-Logo

---
+++ src/components/Sitelogo.js
@@ -0,0 +1,10 @@
+import './Sitelogo.css';
+
+export default function Sitelogo() {
+    return (
+        <span class="logo-text">
+            <i class="fa fa-paw" aria-hidden="true"></i>
+            <span class="big-letter">A</span>ni<span class="big-letter">M</span>ania
+        </span>
+    );
+}
